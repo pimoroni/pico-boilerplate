4afe4622de2f13e82ca2d0bf397f2ad5a4c5e447
Phil Howard <phil@gadgetoid.com>
2021-01-22 13:13:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pico C++ Boilerplate Project
+
+This project is intended as a starting point for working with the Pico SDK and Pimoroni Libraries in C++.
+
+
+You should begin by ensuring your `PICO_SDK_PATH` environment variable is set, and then (assuming pico-sdk is alongside your project):
+
+```
+ln -s ../pico-sdk/external/pico_sdk_import.cmake .
+```  
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "pico_explorer.hpp"
+
+using namespace pimoroni;
+
+uint16_t buffer[PicoExplorer::WIDTH * PicoExplorer::HEIGHT];
+PicoExplorer pico_explorer(buffer);
+
+int main() {
+    pico_explorer.init();
+
+    // set the backlight to a value between 0 and 255
+    // the backlight is driven via PWM and is gamma corrected by our
+    // library to give a gorgeous linear brightness range.
+    pico_explorer.set_backlight(100);
+
+    pico_explorer.set_pen(255, 0, 0);
+
+    while(true) {
+        pico_explorer.pixel(point(0, 0));
+        // now we've done our drawing let's update the screen
+        pico_explorer.update();
+    }
+}
